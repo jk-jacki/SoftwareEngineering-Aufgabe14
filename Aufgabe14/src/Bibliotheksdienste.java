9991eb7dc708e1a4f7a94a2d62fe35c609f31b58
Jacqueline Koch <jacqueline.koch@hs-furtwangen.de>
2022-06-23 13:28:46 +0200

    Implemented method to choose task

--- Aufgabe14/src/Bibliotheksdienste.java
+++ Aufgabe14/src/Bibliotheksdienste.java
@@ -1,28 +1,73 @@
 import java.util.Date;
 
 public class Bibliotheksdienste implements UserInterface{
+
+    public void main(String[] args){
+        int aktion = chooseAction();
+
+        switch (aktion){
+            case 1 :
+                addNewAusleiher();
+                break;
+            case 2:
+                addToBibliotheksbestand();
+                break;
+            case 3:
+                borrowBook();
+                break;
+            case 4:
+                extendBook();
+                break;
+            case 5:
+                returnBook();
+                break;
+            default:
+                System.out.println("Sie haben eine invalide Eingabe getätigt, bitte starten Sie das Programm neu.");
+                break;
+        }
+    }
+
     @Override
-    public Ausleiher ausleiherAnlegen() {
+    public int chooseAction() {
+        System.out.println("Bitte wählen Sie eine der folgenden Aufgaben: ");
+        System.out.println("1: Eine ausleihberechtigte Person anlegen");
+        System.out.println("2: Medium in den Bibliotheksbestand aufnehmen");
+        System.out.println("3: Buch ausleihen");
+        System.out.println("4: Buch verlängern");
+        System.out.println("5: Buch zurückgeben");
+
+        int input = 0;
+        if (scanner.hasNextInt())
+            input = scanner.nextInt();
+        else
+            System.out.println("Invalide Eingabe, bitte starten Sie das Programm neu.");
+        return input;
+    }
+
+    @Override
+    public Ausleiher addNewAusleiher() {
         return null;
     }
 
     @Override
-    public void hinzufügenBibliotheksbestand() {
+    public void addToBibliotheksbestand() {
 
     }
 
     @Override
-    public boolean ausleihenBuch() {
+    public boolean borrowBook() {
         return false;
     }
 
     @Override
-    public Date verlängereBuch() {
+    public Date extendBook() {
         return null;
     }
 
     @Override
-    public boolean rückgabeBuch() {
+    public boolean returnBook() {
         return false;
     }
+
+
 }
--- Aufgabe14/src/UserInterface.java
+++ Aufgabe14/src/UserInterface.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public interface UserInterface {
     Scanner scanner = new Scanner(System.in);
 
-    abstract Ausleiher ausleiherAnlegen();
-    abstract void hinzufügenBibliotheksbestand();
-    abstract boolean ausleihenBuch();
-    abstract Date verlängereBuch();
-    abstract boolean rückgabeBuch();
+    int chooseAction();
+    Ausleiher addNewAusleiher();
+    void addToBibliotheksbestand();
+    boolean borrowBook();
+    Date extendBook();
+    boolean returnBook();
 }
